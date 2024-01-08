99a14a8c5e5529b3dac166b96212fd54c85e4863
Mykhailo Machavariani <rikomydog@gmail.com>
2024-01-08 13:37:27 +0200

    shortened the code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("enter a binary number up to 8 digits")
 binary = input()
 dec_arr = []
 for i in range(len(binary)):
-        dec_arr.append(pow(int(binary[i])*2,-1*(i-(len(binary)-1))))
+        dec_arr.append(pow(int(binary[i])*2,len(binary)-1-i))
 if int(binary[-1]) == 0:
     dec_arr[-1] = 0
 print(sum(dec_arr))
